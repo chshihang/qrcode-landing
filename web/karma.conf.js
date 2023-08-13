2fe75dfe04f70bd48d125825db94ca846900fcd4
chenshihang <858190647@qq.com>
2023-08-13 17:34:42 +0800

    update

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/healthTraining'),
+      dir: require('path').join(__dirname, './coverage/web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
@@ -33,10 +33,6 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
@@ -44,7 +40,6 @@ module.exports = function (config) {
         flags: ['--no-sandbox']
       }
     },
-    singleRun: false,
     restartOnFileChange: true
   });
 };
