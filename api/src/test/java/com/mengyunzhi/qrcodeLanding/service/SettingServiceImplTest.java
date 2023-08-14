615bda1d5a1c55018d9fac58bb25c4cc6ffa3afb
chenshihang <858190647@qq.com>
2023-08-14 23:53:32 +0800

    update

--- api/src/test/java/com/mengyunzhi/qrcodeLanding/service/SettingServiceImplTest.java
+++ api/src/test/java/com/mengyunzhi/qrcodeLanding/service/SettingServiceImplTest.java
@@ -1,13 +1,18 @@
 package com.mengyunzhi.qrcodeLanding.service;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SettingServiceImplTest {
+  Logger logger = LoggerFactory.getLogger(this.getClass());
+
   @Autowired
   SettingServiceImpl settingServiceImpl;
   @Test
@@ -15,9 +20,13 @@ class SettingServiceImplTest {
     String url1 = "http://www.example.com:80/path/to/resource?query=parameters#fragment";
     String url2 = "https://www.example.com/path/to/resource";
     String url3 = "ftp://ftp.example.com/files";
+    String url4 = "http";
 
     assertTrue(this.settingServiceImpl.checkHttpUrl(url1));
     assertTrue(this.settingServiceImpl.checkHttpUrl(url2));
     assertFalse(this.settingServiceImpl.checkHttpUrl(url3));
+
+    logger.info(String.valueOf(this.settingServiceImpl.checkHttpUrl(url4)));
+
   }
 }
