9116adf9c14c51e8442ec412386ec015ed35b720
chenshihang <858190647@qq.com>
2023-08-15 09:13:24 +0800

    update profile

--- api/src/test/java/com/mengyunzhi/qrcodeLanding/service/SettingServiceImplTest.java
+++ api/src/test/java/com/mengyunzhi/qrcodeLanding/service/SettingServiceImplTest.java
@@ -5,11 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("demo")
 class SettingServiceImplTest {
   Logger logger = LoggerFactory.getLogger(this.getClass());
 
