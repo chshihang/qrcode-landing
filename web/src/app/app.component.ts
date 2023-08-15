d32446d54edbf45575b0afa52a762f679518b5db
chenshihang <858190647@qq.com>
2023-08-15 19:03:49 +0800

    update

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {UserService} from "../service/user.service";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {CommonService} from "../service/common.service";
+import {HttpErrorInterceptor} from "../interceptor/http-error.interceptor";
 
 @Component({
   selector: 'app-root',
@@ -11,10 +14,28 @@ export class AppComponent implements OnInit {
   title = 'web';
 
   constructor(private router: Router,
+              private injector: Injector,
+              private commonService: CommonService,
               private userService: UserService) {
   }
 
   ngOnInit(): void {
+    const httpInterceptors = this.injector.get(HTTP_INTERCEPTORS);
+    httpInterceptors.forEach(httpInterceptor => {
+      if (httpInterceptor instanceof HttpErrorInterceptor) {
+        httpInterceptor.error = (url, message) => {
+          this.commonService.error(() => {
+          }, url, message);
+        };
+        httpInterceptor.goToLoginPath = () => {
+          if (this.router && this.router.url && !this.router.url.startsWith(`/login`)) {
+            this.router.navigateByUrl('/login').then();
+          }
+        }
+      }
+    })
+
+
     if (this.router && this.router.url && !this.router.url.startsWith(`/login`)) {
       this.userService.initCurrentLoginUser(() => {
       }).subscribe({
