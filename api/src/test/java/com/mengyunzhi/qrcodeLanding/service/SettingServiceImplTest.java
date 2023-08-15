3a4112151ce766d00c0c90cb0c5b04e2f62d6766
chenshihang <858190647@qq.com>
2023-08-15 09:13:24 +0800

    update profile

--- api/src/test/java/com/mengyunzhi/qrcodeLanding/service/SettingServiceImplTest.java
+++ api/src/test/java/com/mengyunzhi/qrcodeLanding/service/SettingServiceImplTest.java
@@ -3,10 +3,12 @@ package com.mengyunzhi.qrcodeLanding.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("demo")
 class SettingServiceImplTest {
   @Autowired
   SettingServiceImpl settingServiceImpl;
