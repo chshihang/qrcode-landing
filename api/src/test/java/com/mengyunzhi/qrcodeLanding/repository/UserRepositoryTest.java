85960e6bdc32f4cb5c89d700ab8ef70913fa2555
chenshihang <858190647@qq.com>
2023-08-15 09:13:24 +0800

    update profile

--- api/src/test/java/com/mengyunzhi/qrcodeLanding/repository/UserRepositoryTest.java
+++ api/src/test/java/com/mengyunzhi/qrcodeLanding/repository/UserRepositoryTest.java
@@ -3,10 +3,11 @@ package com.mengyunzhi.qrcodeLanding.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("demo")
 class UserRepositoryTest {
   @Autowired
   UserRepository userRepository;
